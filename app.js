bfe5bd2d01fd3b85cccafd7a822834c17829a58d
Gabizzix <gabriela2mattos.5@gmail.com>
2023-09-18 18:06:08 -0400

    Lista-3---PTAS

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Loja de Esmaltes' });
+
+  res.render('index');
+});
+
+
+app.get('/', (req, res) => {
+
+  req.render('index');
+  res.render('produto');
+  
 });
 
 app.listen(port, () => {
